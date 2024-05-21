461b1cf5770c8ae73378105b41be5766b4b8b3b6
Mohamed Amine MEZGHICH <git@mezghich.com>
2024-05-21 13:00:58 +0100

    projet .net avec deux entités

---
+++ TaskForceHM_WEBAPI/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskForceHM_WEBAPI.Models
+{
+    public class Message
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Titre { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ TaskForceHM_WEBAPI/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using TaskForceHM_WEBAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<ApplicationContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("connString")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
